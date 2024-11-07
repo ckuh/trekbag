2ca074ba22a53e2aff4da91718e01bed549608e6
ckuh <ck3731@gmail.com>
2024-11-07 00:11:01 -0800

    refactor: render button gruop from array of text

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,12 +1,20 @@
 import Button from "./Button";
 
 export default function ButtonGroup() {
+  const secondaryButtonsText = [
+    "Mark all as complete",
+    "Mark all as incomplete",
+    "Reset to initial",
+    "Remove all items",
+  ];
+
   return (
     <section className="button-group">
-      <Button type="secondary">Mark all as complete</Button>
-      <Button type="secondary">Mark all as incomplete</Button>
-      <Button type="secondary">Reset to initial</Button>
-      <Button type="secondary">Remove all items</Button>
+      {secondaryButtonsText.map((text) => (
+        <Button key={text} type="secondary">
+          {text}
+        </Button>
+      ))}
     </section>
   );
 }
